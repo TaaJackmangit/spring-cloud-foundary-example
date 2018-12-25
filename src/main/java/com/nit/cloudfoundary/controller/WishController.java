5ed0badc91b1db5822c60bd784cbd0786417f57d
TaaJackmangit <41802549+TaaJackmangit@users.noreply.github.com>
2018-12-26 02:26:17 +0530

    Add files via upload

---
+++ src/main/java/com/nit/cloudfoundary/controller/WishController.java
@@ -0,0 +1,19 @@
+package com.nit.cloudfoundary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WishController {
+
+	@GetMapping("/wishMe")
+	public String greeting(@RequestParam("name") String name, Model model) {
+
+		String message = "Hi " + name + " Welcome to cloud foundary";
+		model.addAttribute("message", message);
+		return "home";
+	}
+
+}
